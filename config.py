ad6214336312ef1b53f21ef1a4a465b480d9ef17
Tore Aadland <tore.aad@gmail.com>
2019-05-10 07:29:09 +0000

    Steps taken to preprocess data

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-
-train_dir = 'data/train'
-val_dir   = 'data/validate'
-test_dir  = 'data/test'
+train_dir = '/home/jupyter/plankton-siamese/train'
+val_dir   = '/home/jupyter/plankton-siamese/validate'
+test_dir  = '/home/jupyter/plankton-siamese/test'
 
 batch_size = 20
 logfile    = 'train.log'
@@ -10,3 +9,4 @@ last       = 0
 learn_rate = 0.01
 lr_decay   = 0.9
 iterations = 5
+
