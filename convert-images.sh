96b7ca7eb9016d9a56553d8d352f65845e5d9e50
Tore Aadland <tore.aad@gmail.com>
2019-05-10 13:51:38 +0000

    Debugging template

--- convert-images.sh
+++ convert-images.sh
@@ -6,21 +6,22 @@ shopt -s failglob
 # resize to 299x299, but keep image size
 # drop -resize if zooming with crop is better (but maybe not?)
 
-BASE=./imgs
-OUT=./data
-CWD=$(pwd)
+export BASE=ZooScanSet/imgs
+export OUT=data
+export CWD=$(pwd)
 
 convert_file(){
-    f = $1
+    f=$1
     echo "$CWD/$BASE/$DIR/$f" "$CWD/$OUT/$DIR/$f"
 }
+export convert_file
 
 convert_dir(){
-    DIR = $1
+    export DIR=$1
     #echo "$DIR"
     #mkdir -p "$OUT/$DIR"
-
-    parallel convert_file ::: $(ls -1 "$BASE/$DIR")
+    echo "Starting parallel convert_file"
+    parallel convert_file ::: $(ls -1 "$CWD/$BASE/$1" | head)
 
     # ls "$BASE/$DIR" | while read f; do 
     #     echo "$CWD/$BASE/$DIR/$f" "$CWD/$OUT/$DIR/$f"
@@ -28,4 +29,6 @@ convert_dir(){
         # convert -resize 299x299 "$CWD/$BASE/$DIR/$f" -background white -gravity center -extent 299x299 "$CWD/$OUT/$DIR/$f"
     # done
 }
-parallel convert_dir ::: $(ls -1 $BASE)
+export convert_dir
+echo "Starting parallel convert_dir"
+parallel convert_dir ::: $(ls -1 $BASE | head)
