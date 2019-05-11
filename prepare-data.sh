9ac859acd14ce1b164810617818b35fe513d6982
Tore Aadland <tore.aad@gmail.com>
2019-05-11 19:07:21 +0000

    Small debugging

--- prepare-data.sh
+++ prepare-data.sh
@@ -1,33 +1,34 @@
 
 # sort data into train/validate/test directories
-SET="train validate test"
+export SET="train validate test"
 
-tmp=$(mktemp)
-cwd=$(pwd)
-data=data/
+export cwd=$(pwd)
+export data=data/
 
 prepare_dir(){
     export dir=$1
     echo $dir
     for s in $SET; do 
-        mkdir -p "$s/$dir"
+        mkdir -p "$cwd/$s/$dir"
     done
+    
+    export tmp=$(mktemp)
     ls "$data/$dir" | shuf > $tmp
 
     head -100 $tmp | while read f; do
-        ln -s "$cwd/$data/$dir/$f" "validate/$dir/$f"
+        ln -s "$cwd/$data/$dir/$f" "$cwd/validate/$dir/$f"
     done
     tail -n +101 $tmp | head -100 | while read f; do
-        ln -s "$cwd/$data/$dir/$f" "test/$dir/$f"
+        ln -s "$cwd/$data/$dir/$f" "$cwd/test/$dir/$f"
     done
     tail -n +201 $tmp | while read f; do
-        ln -s "$cwd/$data/$dir/$f" "train/$dir/$f"
+        ln -s "$cwd/$data/$dir/$f" "$cwd/train/$dir/$f"
     done
 
     rm $tmp
 }
 export -f prepare_dir
 
- parallel -u --eta prepare_dir ::: $(ls -1 $data)
+parallel -u --eta prepare_dir ::: $(ls -1 $data)
 
 
