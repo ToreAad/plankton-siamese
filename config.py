03cc8a628874b13718505730d1b9f20a0ea2e91d
ToreAad <tore.aad@gmail.com>
2019-05-13 12:41:59 +0200

    Add parameters for base and siamese network seperately

--- config.py
+++ config.py
@@ -1,16 +1,26 @@
+# Input data parameters
 train_dir = 'train'
 val_dir = 'validate'
 test_dir = 'test'
-batch_size = 8
+in_dim = (299, 299, 1)
+out_dim = 64
+n_classes = 40
+
+# Base network parameters
+base_model = "simple_convolutional"
+base_batch_size = 8
+base_steps_per_epoch = 800//base_batch_size
+base_validation_steps = 800//base_batch_size
+base_epochs = 1
+
+# Siamese network parameters
+siamese_batch_size = 8
+siamese_steps_per_epoch = 8000//base_batch_size
+siamese_validation_steps = 800//base_batch_size
+siamese_epochs = 10
 logfile = 'train.log'
 learn_rate = 0.01
 lr_decay = 0.9
 iterations = 5
-steps_per_epoch = 2000
-val_per_epoch = 100
-epochs = 5
-base_model = "simple_convolutional"
 last = 0
-in_dim = (299, 299, 3)
-out_dim = 64
-n_classes = 40
+
